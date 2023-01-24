edd29ad228576065fdbf62a8d27e330e354fc8f5
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-24 15:48:28 +0300

    - Reverted changes

--- shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/constants/SharedModuleConstants.kt
+++ shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/constants/SharedModuleConstants.kt
@@ -9,7 +9,3 @@ object KtorConstants {
     const val SPACEX_API_BASE_URL = "https://api.spacexdata.com/v5/"
     const val SPACEX_LAUNCHES_ENDPOINT = "launches"
 }
-
-object TestConstants {
-    const val TEST_CONSTANT_1 = "testConstant1"
-}
