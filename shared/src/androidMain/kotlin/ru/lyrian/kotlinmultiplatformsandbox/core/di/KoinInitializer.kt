fb3439b20f0d44a4ce523c329ca827e90e27e308
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-30 12:25:54 +0300

    - Fixed wrong KoinInitializer.kt decladation in androidMain source set

--- shared/src/androidMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/di/KoinInitializer.kt
+++ shared/src/androidMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/di/KoinInitializer.kt
@@ -3,6 +3,6 @@ package ru.lyrian.kotlinmultiplatformsandbox.core.di
 import org.koin.core.KoinApplication
 import org.koin.dsl.koinApplication
 
-actual class KoinInitializer(koinApplication: KoinApplication.() -> Unit) {
-    actual fun initializeKoin() = initializeSharedGraph { koinApplication() }
+actual class KoinInitializer(private val koinApplication: KoinApplication.() -> Unit) {
+    actual fun initializeKoin() = initializeSharedGraph(koinApplication = koinApplication)
 }
