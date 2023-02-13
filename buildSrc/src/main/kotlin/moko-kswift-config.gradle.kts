3ef36ab9f530d4ea5e189c58426ae2c8372867dc
lapin <alapin@hashinc.tech>
2023-02-13 20:14:15 +0300

    update filter for kswift plugin

--- buildSrc/src/main/kotlin/moko-kswift-config.gradle.kts
+++ buildSrc/src/main/kotlin/moko-kswift-config.gradle.kts
@@ -8,6 +8,6 @@ plugins {
 // Generating from ResourseState (Sealed Class) -> ResourceStateKs (Swift Enum class)
 kswift {
     install(dev.icerock.moko.kswift.plugin.feature.SealedToSwiftEnumFeature) {
-        filter = includeFilter("ClassContext/dev.icerock.moko:mvvm-state/dev/icerock/moko/mvvm/ResourceState")
+        filter = includeFilter("ClassContext/dev.icerock.moko:mvvm-state/dev/icerock/moko/mvvm/ResourceState", "ClassContext/ru.lyrian.kotlinmultiplatformsandbox:shared/ru/lyrian/kotlinmultiplatformsandbox/feature/responce_validation_test/KtorErrors")
     }
 }
