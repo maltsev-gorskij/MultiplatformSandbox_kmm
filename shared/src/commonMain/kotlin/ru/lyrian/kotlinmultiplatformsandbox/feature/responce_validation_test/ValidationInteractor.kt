c007984e554c8c192f920c6f0515c0266ad46399
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-02-14 11:26:27 +0300

    KMM-34: -

--- shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/feature/responce_validation_test/ValidationInteractor.kt
+++ shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/feature/responce_validation_test/ValidationInteractor.kt
@@ -1,8 +1,8 @@
 package ru.lyrian.kotlinmultiplatformsandbox.feature.responce_validation_test
 
-import dev.icerock.moko.mvvm.ResourceState
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.resources.get
+import io.ktor.client.statement.bodyAsText
 import io.ktor.resources.Resource
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -31,27 +31,55 @@ class ValidationInteractor : KoinComponent {
         httpClient.get(Get())
     }
 
-    suspend fun throwSomeException(): ResourceState<String, Throwable> {
-        var resourceState: ResourceState<String, Throwable> = ResourceState.Empty()
-        runCatching {
-            throw KtorErrors.ClientRequest(
-                errorCode = 404,
-                errorMessage = "Dangerous error"
-            )
-        }.onSuccess {
-            resourceState = ResourceState.Success("123")
-        }.onFailure {
-            resourceState = ResourceState.Failed(it)
-        }
-
-        return resourceState
+    suspend fun getNetworkError(): KmmResult<String, Throwable> {
+        return runCatching {
+            httpClient.get(Status.ClientError400()).bodyAsText()
+        }.produceKmmResult()
     }
+
+    suspend fun getNetworkSuccess(): KmmResult<String, Throwable> =
+        runCatching { httpClient.get(Get()).bodyAsText() }.produceKmmResult()
 }
 
-sealed class KmmResult<R, E> {
-    class Success<R, E>(val data: R) : KmmResult<R, E>()
-    class Exception<R, E>(val exception: E) : KmmResult<R, E>()
+internal fun <T : Any> Result<T>.produceKmmResult(): KmmResult<T, Throwable> {
+    val output = this.getOrNull()
+    val exception = this.exceptionOrNull()
+
+    if (output != null) {
+        return KmmResult.Success(output)
+    } else if (exception != null) {
+        return KmmResult.Exception(exception)
+    }
+
+    throw KmmResultProduceException("Cannot process Result")
 }
+
+sealed class KmmResult<R: Any, E: Any> {
+    class Success<R : Any, E : Any>(val data: R) : KmmResult<R, E>()
+    class Exception<R : Any, E : Any>(val exception: E) : KmmResult<R, E>()
+}
+
+inline fun <R : Any, T : Any> KmmResult<R, T>.onSuccess(action: (value: R) -> Unit): KmmResult<R, T> {
+    if (this is KmmResult.Success<R, T>) action(this.data)
+
+    return this
+}
+
+inline fun <R : Any, T : Any> KmmResult<R, T>.onException(action: (exception: T) -> Unit): KmmResult<R, T> {
+    if (this is KmmResult.Exception<R, T>) action(this.exception)
+
+    return this
+}
+
+class KmmResultProduceException(message: String) : Exception(message)
+
+sealed class KtorErrors(val errorCode: Int, val errorMessage: String): Exception(errorMessage) {
+    class RedirectResponse(errorCode: Int, errorMessage: String) : KtorErrors(errorCode, errorMessage)
+    class ClientRequest(errorCode: Int, errorMessage: String) : KtorErrors(errorCode, errorMessage)
+    class ServerResponse(errorCode: Int, errorMessage: String) : KtorErrors(errorCode, errorMessage)
+    class GeneralNetworkError(errorCode: Int, errorMessage: String) : KtorErrors(errorCode, errorMessage)
+}
+
 
 /*sealed class ResourceState<out T, out E> {
     data class Success<out T, out E>(val data: T) : ResourceState<T, E>()
