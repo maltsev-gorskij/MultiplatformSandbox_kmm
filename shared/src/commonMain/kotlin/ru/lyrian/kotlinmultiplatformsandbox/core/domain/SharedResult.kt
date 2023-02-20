da07d72339ce290edc3a3c0f20af72578a786254
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-02-20 11:19:31 +0300

    KMM-90: - Moved onSuccess/onException android platform methods to androidMain source set - Added isSuccess/isException function to SharedResult

--- shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/domain/SharedResult.kt
+++ shared/src/commonMain/kotlin/ru/lyrian/kotlinmultiplatformsandbox/core/domain/SharedResult.kt
@@ -3,6 +3,10 @@ package ru.lyrian.kotlinmultiplatformsandbox.core.domain
 sealed class SharedResult<R: Any, E: Any> {
     class Success<R : Any, E : Any>(val data: R) : SharedResult<R, E>()
     class Exception<R : Any, E : Any>(val exception: E) : SharedResult<R, E>()
+
+    fun isSuccess(): Boolean = this is Success
+
+    fun isException(): Boolean = this is Exception
 }
 
 internal fun <T : Any> Result<T>.asSharedResult(): SharedResult<T, Throwable> {
@@ -16,18 +20,6 @@ internal fun <T : Any> Result<T>.asSharedResult(): SharedResult<T, Throwable> {
     }
 
     throw SharedResultProduceException("Cannot process Result")
-}
-
-inline fun <R : Any, T : Any> SharedResult<R, T>.onSuccess(action: (value: R) -> Unit): SharedResult<R, T> {
-    if (this is SharedResult.Success<R, T>) action(this.data)
-
-    return this
-}
-
-inline fun <R : Any, T : Any> SharedResult<R, T>.onException(action: (exception: T) -> Unit): SharedResult<R, T> {
-    if (this is SharedResult.Exception<R, T>) action(this.exception)
-
-    return this
 }
 
 class SharedResultProduceException(message: String) : Exception(message)
